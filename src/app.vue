a9b3e1dffb92d227158f5d3dbbf4df2788b86e4b
李培汉 <lipeihan@chandashi.com>
2018-10-31 14:24:49 +0800

    inital commit

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <tabs />
+  </div>
+</template>
+
+<script>
+import Tabs from "./tabs";
+
+export default {
+  name: "app",
+  components: {
+    Tabs
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '../node_modules/nib/index.styl'
+
+normalize-css()
+$color-theme = rgb(212, 68, 57)
+
+.app
+  color: red
+</style>
---
+++ src/tabs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tabs">
+    <div class="tabs-titles">
+      <div class="title-item"
+           v-for="item in tabs"
+           :key="item.name">
+        {{item.name}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabs: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
