5739e2062a5dc5a2316d3e638c7a4d61d90874d2
lipeihan <291630202@qq.com>
2018-11-01 21:41:38 +0800

    perf: 修改测试用例

--- test/tabs.test.js
+++ test/tabs.test.js
@@ -1,24 +1,21 @@
 import { mount } from "@vue/test-utils";
-import Tabs from "./src/tabs.vue";
+import Tabs from "../src/tabs.vue";
 
 describe("vue-slide-tabs", () => {
   const wrapper = mount(Tabs, {
     propsData: {
       tabs: [
-        {
-          label: "关注"
-        },
-        {
-          label: "推荐"
-        },
-        {
-          label: "最新"
-        }
+        { label: "关注" },
+        { label: "推荐" },
+        { label: "最新" }
       ]
     }
   });
 
-  console.log("33333dd", wrapper.vm.tabs.length);
+  const tabs = wrapper.vm.$options.props.tabs;
+  it('tabs is of type array', () => {
+    expect(tabs.type).toBe(Array);
+  })
 
   it("The title's length should be 3", () => {
     const divArray = wrapper.findAll("div.title-item");
